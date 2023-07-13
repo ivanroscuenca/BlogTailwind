502e44eb903cd4dca35241988daaf4109bebee0c
ivanroscuenca <ivanros@protonmail.com>
2023-07-13 19:22:27 +0200

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ["Sansita", "sans-serif"],
+        body: ["Lato", "sans-serif"],
+      },
+      colors: {
+        pri: "#081F4D",
+        sec: "#0083FF",
+        gen: "#3B4D71",
+        "our-bg": "#D9E6FF",
+      },
+    },
+  },
+  plugins: [],
+};
